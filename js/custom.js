c3a16b41ffeb508628002841979bc739f23c8aff
Dhiman kumbhakar <dhimankumbhakar0@gmail.com>
2022-07-02 15:54:29 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){"use strict";var header=$('.header');var hamburgerBar=$('.hamburger_bar');var hamburger=$('.hamburger');setHeader();$(window).on('resize',function(){setHeader();setTimeout(function(){$(window).trigger('resize.px.parallax');},375);});$(document).on('scroll',function(){setHeader();});initVideo();initDatePicker();initTimePicker();initMenu();initTestSlider();function setHeader(){if($(window).scrollTop()>91){header.addClass('scrolled');hamburgerBar.addClass('scrolled');}
+else{header.removeClass('scrolled');hamburgerBar.removeClass('scrolled');}}
+function initMenu(){if($('.menu').length){var menu=$('.menu');hamburger.on('click',function(){hamburger.toggleClass('active');menu.toggleClass('active');});}}
+function initVideo(){$(".vimeo").colorbox({iframe:true,innerWidth:640,innerHeight:409,maxWidth:'90%'});}
+function initDatePicker(){var dp=$('#datepicker');var date=new Date();var dateM=date.getMonth()+1;var dateD=date.getDate();var dateY=date.getFullYear();var dateFinal=dateM+'/'+dateD+'/'+dateY;dp.val(dateFinal);dp.datepicker();}
+function initTimePicker(){$('.timepicker').timepicker({interval:60,minTime:'10',maxTime:'6:00pm',defaultTime:'11',startTime:'10:00',dynamic:true,dropdown:true,scrollbar:true});}
+function initTestSlider(){if($('.test_slider').length){var testSlider=$('.test_slider');testSlider.owlCarousel({items:1,loop:true,autoplay:true,dots:true,nav:false,smartSpeed:1200});}}});
